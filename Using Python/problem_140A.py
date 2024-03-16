38268eeeef456e64916dd79d21dd1df1734bdf27
MD. Fazle Rabbi Riyad <135745417+RiyadAndroidDev@users.noreply.github.com>
2024-03-16 23:34:10 +0600

    New Problem Solved

---
+++ Using Python/problem_140A.py
@@ -0,0 +1,13 @@
+t = int(input())
+
+for _ in range(t):
+    a, b = map(int, input().split())
+    if b < a:
+        a, b = b, a
+    sub = b - a
+    div = sub // 10
+    mod = sub % 10
+    sum_val = div
+    if mod > 0:
+        sum_val += 1
+    print(sum_val)
