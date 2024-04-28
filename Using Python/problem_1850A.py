8f88b9c58278508bae69d8b4a25317287e0818f8
MD. Fazle Rabbi Riyad <135745417+RiyadAndroidDev@users.noreply.github.com>
2024-04-28 15:40:34 +0600

    New Problem solved

---
+++ Using Python/problem_1850A.py
@@ -0,0 +1,13 @@
+def main():
+    t = int(input())
+
+    for _ in range(t):
+        a, b, c = map(int, input().split())
+
+        if (a + b) >= 10 or (a + c) >= 10 or (b + c) >= 10:
+            print("YES")
+        else:
+            print("NO")
+
+if __name__ == "__main__":
+    main()
